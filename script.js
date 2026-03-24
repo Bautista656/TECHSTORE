44bd8292e216fa8a8825dcdf384c15a9f18dd470
Jose24Piza <josepiza2444@gmail.com>
2026-03-24 11:19:49 -0600

    style: aplica funcionalidad de diseño resposivo al seleccionar filtro

---
+++ script.js
@@ -0,0 +1,11 @@
+// Control de boton seleccionado
+const filterButtons = document.querySelectorAll('.filter-btn');
+
+filterButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Remover clase active de todos
+    filterButtons.forEach(btn => btn.classList.remove('active'));
+    // Agregar clase active al clickeado
+    button.classList.add('active');
+  });
+});
